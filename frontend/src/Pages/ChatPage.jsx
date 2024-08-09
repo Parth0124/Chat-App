fe17fb0731df50f2b8d2b004b1400f4db51b67a8
Abhiraj412 <abhangparth@gmail.com>
2024-08-09 17:43:57 +0530

    rendered data from the api

--- frontend/src/Pages/ChatPage.jsx
+++ frontend/src/Pages/ChatPage.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
+import axios from 'axios'
+
 function ChatPage() {
+
+    const [chats, setChats] = useState([]);
+
+    const fetchChats= async () => {
+        const {data} = await axios.get('/api/chat')
+        setChats(data)
+    }
+
+    useEffect(() => {
+        fetchChats()
+    })
+
   return (
     <div>
-      <h1>Chat Page</h1>
+        {chats.map(chat=> ( 
+            <div key={chat._id}>{chat.chatName}</div>
+        ))}
     </div>
   );
-}
+} 
 
 export default ChatPage;
