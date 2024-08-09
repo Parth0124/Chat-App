23fd6808f278e27eb7a26374e41b480c3684fe22
Abhiraj412 <abhangparth@gmail.com>
2024-08-09 18:31:44 +0530

    started login and signup page

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,8 +1,24 @@
+import { Box, Container, Text } from "@chakra-ui/react";
+
 function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize='4xl' fontFamily="Work sans" color='black'>Talk-A-Tive</Text>
+      </Box>
+      <Box>
+
+      </Box>
+    </Container>
   );
 }
 
