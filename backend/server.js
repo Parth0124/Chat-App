13838db07a64a96c1967667fe0730d46d6fca880
Abhiraj412 <abhangparth@gmail.com>
2024-09-20 10:51:10 +0530

    registerUser functioon complete

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { chats } = require("./data/data");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const userRoutes = require('./routes/userRoutes')
 
 const app = express();
 dotenv.config();
@@ -21,6 +22,8 @@ app.get("/api/chat/:id", (req, res) => {
   const singleChat = chats.find((c) => c._id === req.params.id);
   res.send(singleChat);
 });
+
+app.use('/api/user', userRoutes)
 
 const PORT = process.env.PORT || 4000;
 
