7166cde062cce72adcd5be95122ebda2f1fd4613
Parth0124 <abhangparth@gmail.com>
2024-12-11 23:34:36 +0530

    ready to deploy

--- frontend/src/pages/auth/index.jsx
+++ frontend/src/pages/auth/index.jsx
@@ -155,11 +155,8 @@ export default function Auth() {
   };
 
   return (
-    <div className='relative h-[100vh] w-[100vw] overflow-hidden flex items-center justify-center'>
-    <div className='absolute inset-0 z-0 bg-cover bg-center' 
-         style={{ backgroundImage: 'url(https://img.freepik.com/premium-photo/colorful-business-frame-with-speech-bubbles-icons-white-background-creative-modern-presentations_43969-57581.jpg)', filter: 'blur(3px)' }}>
-    </div>
-    <div className='relative z-10 h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[90vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2'>
+    <div className='relative h-[100vh] w-[100vw] overflow-hidden flex items-center justify-center bg-slate-900'>
+    <div className='relative z-10 h-[80vh] bg-gray-600 border-2 border-white text-opacity-90 shadow-2xl w-[90vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2'>
     <div className='flex flex-col gap-10 items-center justify-center '>
              <div className='flex items-center justify-center flex-col'>
                 <div className='flex items-center justify-center'>
